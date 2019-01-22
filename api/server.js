2ba02f91e8433d5b324091d8bd3314a5a0970b4f
dividedsky <jdlow2@gmail.com>
2019-01-21 20:57:15 -0500

    set up db and register endpoint

--- api/server.js
+++ api/server.js
@@ -1,10 +1,27 @@
 const express = require('express');
+const bcrypt = require('bcryptjs');
+const db = require('../data/dbConfig');
 
 const server = express();
 server.use(express.json());
 
 server.get('/', (req, res) => {
   res.status(200).send('server is running!');
+});
+
+server.post('/api/register', (req, res) => {
+  const creds = req.body;
+
+  // hash password
+  creds.password = bcrypt.hashSync(creds.password, 14);
+
+  // add user
+  db('users')
+    .insert(creds)
+    .then(id => {
+      res.status(201).send(id);
+    })
+    .catch(err => res.status(500).json(err));
 });
 
 module.exports = server;
